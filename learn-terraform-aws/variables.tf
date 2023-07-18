07edd59f6a995e5411459348e1c352eebb052a6e
Neosoft <neosoft@Neosofts-MacBook-Pro-5.local>
2023-07-18 18:49:54 +0530

    initial commit

---
+++ learn-terraform-aws/variables.tf
@@ -0,0 +1,21 @@
+#EC2---------------------------------------------------------------------------------------------------------------------------
+
+variable "ec2-instance-type" {
+}
+variable "ec2-key" {
+}
+variable "ports" {
+}
+variable "name" {
+}
+variable "ec2-sg-description" {
+}
+variable "ec2-sg-name" {
+}
+
+
+
+
+
+
+
